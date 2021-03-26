91b457ec8d51004fa20d4e0cd3433b63671f7fcb
Bilal Baraz <bilalbaraz@windowslive.com>
2021-03-26 23:13:48 +0300

    Add styles and buttonText props

--- src/component/WishoButton/index.js
+++ src/component/WishoButton/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import styles from './styles';
 
-const WishoButton = () => {
+const WishoButton = (props) => {
     return (
-        <View>
-            <Text>a</Text>
+        <View style={styles.container}>
+            <Text style={styles.buttonText}>{props.buttonText}</Text>
         </View>
     );
+}
+
+WishoButton.defaultProps = {
+    buttonText: 'Connect via Wisho',
 }
 
 export default WishoButton;
---
+++ src/component/WishoButton/styles.js
@@ -0,0 +1,21 @@
+import {StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: 10,
+        margin: 5,
+        backgroundColor: '#ff6500',
+        borderWidth: 1,
+        borderColor: 'rgba(255, 255, 255, 0.5)',
+        borderRadius: 10,
+    },
+    buttonText: {
+        color: '#ffffff',
+        fontSize: 20,
+    }
+});
+
+export default styles;
