50ffd98dd82c70e98a1923de5c71ef61e5be5286
cihattascii <cihad@wishoapp.com>
2021-05-05 18:07:45 +0300

    added camera screen

--- src/Component/WishoButton/styles.js
+++ src/Component/WishoButton/styles.js
@@ -32,6 +32,15 @@ const styles = StyleSheet.create({
       borderTopColor: '#dcdeed',
       borderWidth: 0.5
     },
+    cameraModalContainer: {
+      flex: 1,
+      backgroundColor: 'green',
+      height: height,
+      width: width,
+      bottom: 0,
+      position: 'absolute',
+      justifyContent: 'space-between'
+    },
     containerBranch: {
       width: width * 0.93,
       backgroundColor: 'white',
@@ -163,6 +172,170 @@ const styles = StyleSheet.create({
       fontSize: height * 0.025,
       fontWeight: 'bold',
       color: '#f2f2f7',
+    },
+    topContainer: {
+      width: width,
+      height: height * 0.2,
+    },
+    topBrandInfoContainer: {
+      flexDirection: 'row',
+    },
+    avatarContainer: {
+      width: width * 0.15,
+      height: width * 0.15,
+      borderRadius: width * 0.075,
+      margin: width * 0.02
+    },
+    avatar: {
+      width: width * 0.15,
+      height: width * 0.15,
+      borderRadius: width * 0.075,
+    },
+    brandContainer: {
+      justifyContent: 'space-evenly',
+    },
+    brandNameContainer: {
+      flexDirection: 'row',
+      alignItems: 'center',
+      justifyContent: 'space-between',
+    },
+    brandTopName: {
+      color: 'white',
+      fontWeight: 'bold',
+      fontSize: width *0.045
+    },
+    branchTopName: {
+      color: 'white',
+      fontWeight: 'bold',
+      fontSize: width *0.035
+    },
+    liveContainer: {
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+    },
+    live: {
+      flexDirection: 'row',
+      justifyContent: 'space-evenly',
+      width: width * 0.22,
+      height: height * 0.03,
+      borderRadius: 15,
+      alignItems: 'center',
+      backgroundColor: '#007aed'
+    },
+    liveImage: {
+      width: width * 0.05,
+      height: width * 0.05,
+      borderRadius: width * 0.025
+    },
+    liveText: {
+      color: 'white',
+      fontSize: width *0.035
+    },
+    countText: {
+      color: 'white',
+      fontSize: width *0.035
+    },
+    countDownContainer: {
+      width: width * 0.15,
+      height: height * 0.03,
+      borderRadius: 15,
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: 'gray',
+      marginLeft: 5
+    },
+    videoContainer: {
+      width: width *0.37,
+      height: height * 0.3,
+      marginLeft: width * 0.6,
+      marginTop: -(height * 0.078),
+      justifyContent: 'space-between'
+    },
+    bottomContainer: {
+      height: height *0.2,
+      justifyContent: 'space-around',
+    },
+    buttonContainer: {
+      flexDirection: 'row',
+      alignItems: 'center',
+      justifyContent: 'space-evenly',
+      width: width,
+    },
+    actionButtonContainer: {
+      width: width * 0.12,
+      height: width * 0.12,
+      borderRadius: 15,
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+    actionMidButtonContainer: {
+      width: width * 0.18,
+      height: width * 0.18,
+      borderRadius: 15,
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+    messageContainer: {
+      justifyContent: 'space-evenly',
+      width: width,
+      height: height * 0.1,
+      flexDirection: 'row',
+      alignItems: 'center'
+    },
+    messageInput: {
+      width: width * 0.80,
+      height: width * 0.12,
+      borderRadius: 15,
+      backgroundColor: 'rgba(255,255,255,0.25)',
+      paddingLeft: 10
+    },
+    sendButton: {
+      width: width * 0.12,
+      height: width * 0.12,
+      borderRadius: 15,
+      justifyContent: 'center',
+      alignItems: 'center'
+    },
+    basketIcon: {
+      width: width * 0.06,
+      height: width * 0.06,
+    },
+    reportIcon: {
+      width: width * 0.065,
+      height: width * 0.06,
+    },
+    micIcon: {
+      width: width * 0.05,
+      height: width * 0.052,
+    },
+    hangupIcon: {
+      width: width * 0.1,
+      height: width * 0.04,
+    },
+    cameraIcon: {
+      width: width * 0.07,
+      height: width * 0.041,
+    },
+    voxChangeEventContainer: {
+      flexDirection: 'row',
+      justifyContent: 'space-evenly'
+    },
+    changeContainer: {
+      width: (35 * width) / 373,
+      height: (35 * width) / 373,
+      borderRadius: (25 * width) / 373,
+      backgroundColor: 'white',
+      opacity: 0.5,
+      justifyContent: 'center',
+      alignItems: 'center'
+    },
+    changeIcon: {
+      width: width * 0.05,
+      height: width * 0.06,
+    },
+    earIcon: {
+      width: width * 0.07,
+      height: width * 0.038,
     },
 });
 
